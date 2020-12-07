d0f31c6e0de2eb679a8a42ff400e69011b7e9f18
Deuk_Hoi <dh97k@naver.com>
2020-12-07 21:16:41 +0900

    modify faq & Qna & Notice page

--- src/com/webprogramming/project/NoticeDAO.java
+++ src/com/webprogramming/project/NoticeDAO.java
@@ -12,10 +12,10 @@ public class NoticeDAO {
 	public ResultSet rs = null;
 	JSONArray Jarray = new JSONArray();
 	
-	public JSONArray SelectNotice(){
+	public JSONArray SelectNotice(String pages){
 		DatabaseManager db = new DatabaseManager();		
-		sql  = "SELECT * FROM Notice"; 
-		
+		sql  = "SELECT * FROM Notice LIMIT "+(Integer.parseInt(pages)-1)*10+", 10"; 
+		Jarray = new JSONArray();
 		rs = db.selectDB(sql, null);
 		
 		try {
@@ -29,6 +29,7 @@ public class NoticeDAO {
 				obj.put("views", rs.getString("views"));
 				Jarray.add(obj);
 			}
+			rs.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} 
@@ -38,7 +39,7 @@ public class NoticeDAO {
 	
 	public JSONArray SelectNoticeDetail(String num){
 		DatabaseManager db = new DatabaseManager();
-		
+		int views = 0;
 		HashMap<Integer, String> info = new HashMap<Integer, String>();
 		info.put(1, num);
 		
@@ -53,11 +54,43 @@ public class NoticeDAO {
 				obj.put("content", rs.getString("content"));
 				obj.put("photo", rs.getString("photo"));
 				obj.put("date", rs.getString("date"));
+				views = rs.getInt("views");
 				Jarray.add(obj);
 			}
+			
+			info = new HashMap<Integer, String>();
+			sql = "UPDATE Notice SET views = ? WHERE nid = ?";
+			info.put(1, Integer.toString(views+1));
+			info.put(2, num);
+			db.InsertUpdateDB(sql, info);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} 
 		return Jarray;
+	}
+	
+	public int getCount(){
+		DatabaseManager db = new DatabaseManager();		
+		sql  = "SELECT COUNT(*) as cnt FROM Notice"; 
+		Jarray = new JSONArray();
+		rs = db.selectDB(sql, null);
+		int Pagecount = 0;
+		
+		try {
+			while(rs.next()) {
+				Pagecount = rs.getInt("cnt");
+			}
+			
+			if(Pagecount%10==0) {
+				Pagecount = (Pagecount/10)-1;
+			}else {
+				Pagecount = Pagecount/10;
+			}
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return Pagecount;
 	}
 }
